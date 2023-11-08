654e52a4de5b4e3c41c7cae64aef2837c0181305
Kevin <kevinhornbek@gmail.com>
2023-11-08 09:40:27 +0100

    Added "pages" folder

---
+++ pages/CreateNewWorkflowPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CreateNewWorkflowPage = () => {
+    return (
+        <View>
+            <Text>CreateNewWorkflowPage HelloWorld</Text>
+        </View>
+    );
+};
+
+export default CreateNewWorkflowPage;
