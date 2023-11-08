65351b65e0762cf1d09e2805fbfa44a9d4de379a
Kevin <kevinhornbek@gmail.com>
2023-11-08 09:38:33 +0100

    Removed "Pages" folder

--- Pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Page from './Page';
-
-export const AccountPage = ({navigation}) => <Page navigation={navigation} name="Account" />
-export const AssignedWorkflowsPage = ({navigation}) => <Page navigation={navigation} name="AssignedWorkflows" />
-export const CreateNewWorkflowPage = ({navigation}) => <Page navigation={navigation} name="CreateNewWorkflow" />
-export const WorkflowAssignmentsPage = ({navigation}) => <Page navigation={navigation} name="WorkflowAssignments" />
