513b4be9ee493e03ccf47b99973e1f1473b63fed
WetDrywall <Teisvhjelm@gmail.com>
2023-11-10 10:43:33 +0100

    work? YIPIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
    yep

--- components/WorkflowCard.js
+++ components/WorkflowCard.js
@@ -1,27 +1,22 @@
-import { View, Text, StyleSheet, Dimensions, Animated } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Animated, Pressable } from 'react-native';
 import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const WorkflowCard = ({ title, description, deadline }) => {
- const [height] = useState(new Animated.Value(150));
+  const [isPressed, setIsPressed] = useState(false);
+  const handlePress = () => {
+    setIsPressed(isPressed ? false : true);
+  };
 
- const handlePress = () => {
-    console.log(height._value);
-   Animated.timing(height, {
-     toValue: height._value === 150 ? 500 : 150,
-     duration: 300,
-     useNativeDriver: false
-   }).start();
- };
-
- return (
-   <Animated.View style={[styles.card, { height }]} onPress={handlePress}>
-     <Text style={styles.title}>{title}</Text>
-     <Text style={styles.description} numberOfLines={3}>
-       {description?.substring(0, 120)}...
-     </Text>
-     <Text style={styles.date}>{deadline}</Text>
-   </Animated.View>
- );
+  return (
+      <TouchableOpacity style={[isPressed ? styles.pressedCard : styles.card]} onPress={handlePress}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.description} numberOfLines={3}>
+              {description?.substring(0, 120)}...
+          </Text>
+          <Text style={styles.date}>{deadline}</Text>
+      </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -49,6 +44,19 @@ const styles = StyleSheet.create({
     date: {
         fontSize: 16,
         fontWeight: 'bold',
+    },
+    pressedCard: {
+      height: 300,
+      width: Dimensions.get('window').width * 0.95,
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      padding: 10,
+      margin: 5,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      shadowRadius: 5,
+      elevation: 3,
     },
 });
 
