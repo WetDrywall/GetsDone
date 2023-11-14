67865d7223e1326a43dd3dc816852bc853e3e3ff
Kevin Hornbek-Knudsen <kevinhornbek@gmail.com>
2023-11-14 12:22:49 +0100

    App design WIP

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
-import { Dimensions } from 'react-native';
+import { Dimensions, Text } from 'react-native';
 
 import AssignedWorkflowsPage from './pages/AssignedWorkflowsPage';
 import CreateNewWorkflowPage from './pages/CreateNewWorkflowPage';
 import AccountPage from './pages/AccountPage';
 
 import SideBar from './components/SideBar';
+import { fgColor } from './components//Colors';
 
 const Drawer = createDrawerNavigator();
 
@@ -16,9 +17,30 @@ export default function App() {
  return (
    <NavigationContainer>
      <Drawer.Navigator drawerContent={props => <SideBar {...props} drawerWidth={Dimensions.get("window").width * 0.75} />}>
-       <Drawer.Screen name="My Workflows" component={AssignedWorkflowsPage} options={{drawerIcon: ({ tintColor }) => <Feather name="archive" size={16} color={tintColor} />}} />
-       <Drawer.Screen name="Create Workflow" component={CreateNewWorkflowPage} options={{drawerIcon: ({ tintColor }) => <Feather name="edit" size={16} color={tintColor} />}} />
-       <Drawer.Screen name="Account" component={AccountPage} options={{drawerIcon: ({ tintColor }) => <Feather name="user" size={16} color={tintColor} />}} />
+       <Drawer.Screen
+          name="My Workflows"
+          component={AssignedWorkflowsPage}
+          options={{
+            drawerIcon: () => <Feather name="archive" size={16} color={fgColor} />,
+            drawerLabel: ({ focused }) => <Text style={{ color: focused ? '#4d79ff' : fgColor }}>My Workflows</Text>
+          }}
+       />
+       <Drawer.Screen
+          name="Create Workflow"
+          component={CreateNewWorkflowPage}
+          options={{
+            drawerIcon: () => <Feather name="edit" size={16} color={fgColor} />,
+            drawerLabel: ({ focused }) => <Text style={{ color: focused ? '#4d79ff' : fgColor }}>Create Workflow</Text>
+          }}
+          />
+       <Drawer.Screen
+          name="Account"
+          component={AccountPage}
+          options={{
+            drawerIcon: () => <Feather name="user" size={16} color={fgColor} />,
+            drawerLabel: ({ focused }) => <Text style={{ color: focused ? '#4d79ff' : fgColor }}>Account</Text>
+        }}
+          />
      </Drawer.Navigator>
    </NavigationContainer>
  );
