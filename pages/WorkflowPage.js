5de63ca136f990da7f96e38256af1170a2c2cec9
Kevin Hornbek-Knudsen <kevinhornbek@gmail.com>
2023-11-21 09:44:58 +0100

    Added second api call

--- pages/WorkflowPage.js
+++ pages/WorkflowPage.js
@@ -15,12 +15,21 @@ const AssignmentList = ({ navigation }) => {
   const [data2, setData2] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const url = `${apiLink}api/ListWorkflowAssignment?wfid=1&aid>0`;
+  const url = `${apiLink}api/ListWorkflow?wfid=1&uid=1`;
+  const url2 = `${apiLink}api/ListWorkflowAssignment?wfid=1&aid>0`;
 
   useEffect(() => {
     fetch(url)
       .then((resp) => resp.json())
       .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    fetch(url2)
+      .then((resp) => resp.json())
+      .then((json) => setData2(json))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
@@ -33,31 +42,33 @@ const AssignmentList = ({ navigation }) => {
       {loading ? (
         <Text>Loading...</Text>
       ) : (
-        data.map((post) => {
-          return (
-            <>
+        <>
+          {data.map((post) => {
+            return (
               <View style={styles.topBox}>
                 <Text style={styles.title}>{post.title}</Text>
                 <Text style={styles.description}>{post.description}</Text>
               </View>
-              {
-                <View style={styles.bottomBox}>
-                  {/* <Text>{post.atitle}</Text> */}
-                  {/* <FlatList
-                    data={post.atitle}
-                    renderItem={({ item }) => <Text>{item.atitle}</Text>}
-                    keyExtractor={(item) => item.aid}
-                  /> */}
-                  {/* <FlatList
-                    data={completedAssignments}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.id}
-                  /> */}
-                </View>
-              }
-            </>
-          );
-        })
+            );
+          })}
+          {data2.map((post) => {
+            return (
+              <View style={styles.bottomBox}>
+                {/* <Text>{post.atitle}</Text>
+                <FlatList
+                  data={post.atitle}
+                  renderItem={({ item }) => <Text>{item.atitle}</Text>}
+                  keyExtractor={(item) => item.aid}
+                />
+                <FlatList
+                  data={completedAssignments}
+                  renderItem={renderItem}
+                  keyExtractor={(item) => item.id}
+                /> */}
+              </View>
+            );
+          })}
+        </>
       )}
     </View>
   );
@@ -91,20 +102,22 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     elevation: 3,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: fgColor,
-  },
-  description: {
-    fontSize: 16,
-    color: fgColor,
-  },
   assignedUser: {
     fontSize: 16,
     color: fgColor,
   },
-  deadline: {
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: fgColor,
+  },
+  description: {
+    fontSize: 16,
+    flex: 1,
+    color: fgColor,
+  },
+  date: {
     fontSize: 16,
     fontWeight: "bold",
     color: fgColor,
